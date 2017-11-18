9a478252a3d6b2880602f7a328654979ecbe052e
freeser.home <freeser@126.com>
2017-11-18 19:26:11 +0800

    基本完成

--- monitor/server.js
+++ monitor/server.js
@@ -16,33 +16,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 io.on('connection', function (socket) {
   var socketId = socket.id;
   socket.on('order', function (message) {
-    var msg = socket.userName + ': ' + '回执：' + JSON.stringify(message);
-    // io.sockets.sockets[socketId].emit('clientMsg', msg); 给指定的socketId发送消息，适用于在其它connect里面
-    if (socket.groupId) {
-        socket.to(socket.groupId).broadcast.emit('group.emit', msg);
-        //io.sockets.in(socket.groupId).emit('group.emit', msg); //方法1.2
-    } else {
-        socket.emit('clientMsg', msg);
-    }
-  });
-  // 加入群组
-  socket.on('group', function (groupId, userName) {
-    socket.groupId = groupId;
-    socket.userName = userName;
-    socket.join(groupId);
-    socket.to(socket.groupId).broadcast.emit('group.emit', userName + '加入群组'+ groupId +'成功');
-    // io.sockets.in(socket.groupId).emit('group.emit', userName + '加入群组'+ groupId +'成功');
-  });
-});
-
-io.of('/room1').on('connection', function (socket) {
-  socket.on('order', function (message) {
-    var msg = socket.userName + ': ' + '回执：' + JSON.stringify(message);
-    socket.broadcast.emit('group.emit', msg);
-  });
-  // 加入群组
-  socket.on('join', function (userName) {
-    socket.userName = userName;
-    socket.broadcast.emit('group.emit', userName + '加入群组room1成功');
+    var msg = '回执：' + JSON.stringify(message);
+    socket.emit('clientMsg', msg);
   });
 });
